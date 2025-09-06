778a166e029c7d74f246e99caf9660291f1348a1
Amaterasu_31 <sathwikkamath312005@gmail.com>
2025-09-06 12:53:36 +0530

    Update Server.js

--- codesprintt/Backend/Server.js
+++ codesprintt/Backend/Server.js
@@ -89,6 +89,6 @@ app.get('/api/registrations', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
