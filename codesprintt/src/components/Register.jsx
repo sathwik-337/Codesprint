8afb326384cb936dfb1ec40b001b2e036ea2d30a
Amaterasu_31 <sathwikkamath312005@gmail.com>
2025-09-06 12:27:01 +0530

    Update Register.jsx

--- codesprintt/src/components/Register.jsx
+++ codesprintt/src/components/Register.jsx
@@ -54,7 +54,7 @@ const Register = () => {
     };
 
     try {
-      const response = await fetch("http://localhost:5000/api/register", {
+      const response = await fetch("https://codesprint3.onrender.com/api/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
